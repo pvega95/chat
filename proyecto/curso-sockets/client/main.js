73501b4897294c9b5ef7c0a59b990f38d64a9f5f
pvega95 <36274495+pvega95@users.noreply.github.com>
2018-04-02 21:37:51 -0500

    proyecto de paul vega
    Proyecto de chat

---
+++ proyecto/curso-sockets/client/main.js
@@ -0,0 +1,32 @@
+var socket = io.connect('http://192.168.10.104:6677',{'forceNew':true});
+
+socket.on('messages',function(data){
+	console.log(data);
+	render(data);
+});
+
+function render(data){
+	var html= data.map(function(message, index){
+		return (`
+			<div class="message">
+				<strong>${message.nickname}</strong> dice:
+				<p>${message.text}</p>
+			</div>
+		`);
+	}).join('');
+
+	var div_msgs=document.getElementById('messages'); //hace que se visualize lo ultimo escrito en el chat
+	div_msgs.innerHTML = html;
+	div_msgs.scrollTop = div_msgs.scrollHeight;
+}
+
+function addMessage(e){
+	var message = {
+		nickname : document.getElementById('nickname').value,
+		text : document.getElementById('text').value
+	};
+
+	document.getElementById('nickname').style.display = 'none';
+	socket.emit('add-message',message);
+	return false;
+}
---
+++ proyecto/curso-sockets/server/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('client'));
+
+app.get('/hola-mundo', function(req,res){
+	res.status(200).send('Hola Mundo desde una ruta');
+})
+
+var messages = [{
+	id: 1,
+	text: 'Bienvenido al chat privado de PAUL VEGA',
+	nickname: 'Bot-Pvega'
+}];
+
+io.on('connection', function(socket){
+	console.log("El cliente con la ip: "+ socket.handshake.address+ " se ah conectado...");
+	socket.emit('messages',messages);
+
+	socket.on('add-message',function(data){
+		messages.push(data);
+
+		io.sockets.emit('messages',messages); //emite el mensajea todos los usuarios conectaos.
+	});
+
+});
+
+server.listen(6677, function(){
+	console.log('Servidor esta funcionando en http://localhost:6677');
+});
